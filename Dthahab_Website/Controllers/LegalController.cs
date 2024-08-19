9fdf1ac8a35fb04fa0ad77146c054c99e4e1e7ca
Mohammad AlMohammad AlMahmoud <mohammadalmahmoud597@gmail.com>
2024-08-19 19:39:09 +0300

    Add project files.

---
+++ Dthahab_Website/Controllers/LegalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dthahab_Website.Controllers
+{
+    public class LegalController : Controller
+    {
+        // GET: Legal
+        public ActionResult Privacy_Policy()
+        {
+            return View();
+        }
+    }
+}
